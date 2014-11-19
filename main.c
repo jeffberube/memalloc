fd219a37d4eaa59fe0bae29f0b9cfe8a95d569ee
nordiqdigital <nordiqdigital@bitbucket.org>
2014-11-19 16:23:04 -0700

    Backup take 2

--- main.c
+++ main.c
@@ -1,28 +1,26 @@
 /*
  * @Author:	Jeff Berube
- * @Title:	memalloc.c
+ * @Title:	main.c
  *
  * @Description:	Simulates memory allocation inside a kernel.
  *
  */
 
 #include <stdio.h>
-
-struct freeNode {
-	struct range hole;
-	struct freeNode *next;
-};
-
-struct allocNode {
-	struct range allocated;
-	int leaseExpiry;
-	struct allocNode *next;
-
-}
+#include "memalloc.h"
 
 int main() {
 
+	long long clock = 0;
+
+	while (++clock != TIME_LIMIT) {
 	
+		if (clock % REQUEST_INTERVAL == 0)
+			request_memory(clock);
+
+		clean_expired_leases(clock);
+	
+	}	
 
 	return 0;
 }
